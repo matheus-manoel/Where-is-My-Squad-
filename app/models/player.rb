f782df24f88c47da263515e986e56ae8f5fae2ce
Matheus Manoel <matheuscmanoel@gmail.com>
2017-05-26 16:21:16 -0300

    Add validations.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,11 @@
 class Player < ApplicationRecord
+  validates :nick, presence: true, length: { maximum: 20 }
+  validates :description, length: { maximum: 275 }
+  validates :server, presence: true, if: :server_exists?
+  #validates :steam_link, url: true
+
+  def server_exists?
+    ['NA', 'SA', 'EU'].include? server
+  end
+
 end
