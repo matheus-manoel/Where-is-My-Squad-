92bd8e8d9e97256264dbf4e96e0e3600b2f68981
Matheus Manoel <matheuscmanoel@gmail.com>
2017-05-29 22:22:07 -0300

    Add server information index.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,11 +1,11 @@
 class Player < ApplicationRecord
   validates :nick, presence: true, length: { maximum: 20 }
   validates :description, length: { maximum: 275 }
-  validates :server, presence: true, if: :server_exists?
+  validates :server, presence: true, length: { maximum: 2 }#, if: :server_exists?
   validates :steam_link, url: true
 
   def server_exists?
-    ['NA', 'SA', 'EU'].include? server
+    ['NA', 'SA', 'EU', 'AS', 'OC'].include? self[:server]
   end
 
 end
